725bc058779d0836929a04951db62cdee812ffae
Marek Pilecky <public@mefi100feles.cz>
2011-06-03 19:49:23 +0200

    added ContentExtractor + its tests

---
+++ src/main/java/com/github/mefi/jkuuza/parser/ContentExtractor.java
@@ -0,0 +1,114 @@
+package com.github.mefi.jkuuza.parser;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author Marek Pilecky
+ */
+public class ContentExtractor {
+
+	private Document doc;
+
+	/**
+	 * Needs non empty Jsoup Document instance
+	 *
+	 * @param doc Jsoup Document
+	 */
+	public ContentExtractor(Document doc) {
+		this.doc = doc;
+	}
+
+	/**
+	 * Returns description from meta tag
+	 *
+	 * @return description value of content attribute
+	 */
+	public String getMetaDescription() {
+		String description = doc.select("meta[name=description]").first().attr("content");
+
+		return description;
+	}
+
+	/**
+	 * Checks, if header of page contains meta tag with description
+	 *
+	 * @return true if page contains description
+	 */
+	public boolean hasMetaDescription() {
+		Elements elements = doc.head().select("meta[name=description]");
+
+		if (elements.isEmpty() || !elements.first().hasAttr("content")) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Returns keywords from meta tag
+	 *
+	 * @return keywords value of content attribute
+	 */
+	public String getMetaKeywords() {
+		String keywords = doc.select("meta[name=keywords]").first().attr("content");
+
+		return keywords;
+	}
+
+	/**
+	 * Checks, if header of page contains meta tag with keywords
+	 *
+	 * @return true if page contains keywords
+	 */
+	public boolean hasMetaKeywords() {
+		Elements elements = doc.head().select("meta[name=keywords]");
+
+		if (elements.isEmpty() || !elements.first().hasAttr("content")) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Returns charset from meta tag
+	 *
+	 * @return charset value of content attribute
+	 */
+	public String getMetaCharset() {
+		String charset = "";
+		charset = doc.select("meta[http-equiv=content-type]").first().attr("content");
+
+		charset = charset.replace("text/html", "");
+		charset = charset.replace("TEXT/HTML", "");
+		charset = charset.replace("charset", "");
+		charset = charset.replace("CHARSET", "");
+		charset = charset.replace("=", "");
+		charset = charset.replace(";", "");
+		charset = charset.replace(" ", "");
+
+		return charset;
+	}
+
+	/**
+	 * Checks, if header of page contains meta tag with charset
+	 *
+	 * @return true if page contains charset value
+	 */
+	public boolean hasMetaCharset() {
+		Elements elements = doc.head().select("meta[http-equiv=content-type]");
+
+		if (elements.isEmpty() || !elements.first().hasAttr("content")) {
+			return false;
+		}
+		return true;
+	}
+
+	public Document getDoc() {
+		return doc;
+	}
+
+	public void setDoc(Document doc) {
+		this.doc = doc;
+	}
+}
---
+++ src/test/java/com/github/mefi/jkuuza/parser/ContentExtractorTest.java
@@ -0,0 +1,195 @@
+package com.github.mefi.jkuuza.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mefi
+ */
+public class ContentExtractorTest {
+
+	Document doc = null;
+	ContentExtractor extractor = null;
+
+	public ContentExtractorTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() {
+		String htmlSkeleton = "<html><head></head><body></body></html>";
+		String baseUri = "http://example.com";
+		doc = Jsoup.parse(htmlSkeleton, baseUri);
+		extractor = new ContentExtractor(doc);
+	}
+
+	/**
+	 * Test of getMetaDescription method, of class ContentExtractor.
+	 */
+	@Test
+	public void testGetMetaDescription() {
+		System.out.println("getMetaDescription");
+
+		String html = "";
+
+		html = "<meta name=\"description\" content=\"foo\" />";
+		setDocHeader(html);
+		assertEquals("foo", extractor.getMetaDescription());
+
+		html = "<meta name=description content=foo />";
+		setDocHeader(html);
+		assertEquals("foo", extractor.getMetaDescription());
+
+	}
+
+	/**
+	 * Test of getMetaKeywords method, of class ContentExtractor.
+	 */
+	@Test
+	public void testGetMetaKeywords() {
+		System.out.println("getMetaKeywords");
+
+		String html = "";
+
+		html = "<meta name=\"keywords\" content=\"foo\" />";
+		setDocHeader(html);
+		assertEquals("foo", extractor.getMetaKeywords());
+
+		html = "<meta name=\"keywords\" content=\"foo, bar, baz\" />";
+		setDocHeader(html);
+		assertEquals("foo, bar, baz", extractor.getMetaKeywords());
+
+		html = "<meta name=keywords content=foo />";
+		setDocHeader(html);
+		assertEquals("foo", extractor.getMetaKeywords());
+	}
+
+	/**
+	 * Test of getMetaCharset method, of class ContentExtractor.
+	 */
+	@Test
+	public void testGetMetaCharset() {
+		System.out.println("getMetaCharset");
+
+		String html = "";
+
+		html = "<meta http-equiv=\"Content-type\" content=\"text/html; charset=UTF-8\">";
+		setDocHeader(html);
+		assertEquals("UTF-8", extractor.getMetaCharset());
+
+		html = "<META HTTP-EQUIV=\"Content-type\" CONTENT=\"text/html; charset=UTF-8\">";
+		setDocHeader(html);
+		assertEquals("UTF-8", extractor.getMetaCharset());
+
+		html = "<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\">";
+		setDocHeader(html);
+		assertEquals("utf-8", extractor.getMetaCharset());
+	}
+
+	/**
+	 * Test of hasMetaDescription method, of class ContentExtractor.
+	 */
+	@Test
+	public void testHasMetaDescription() {
+		System.out.println("hasMetaDescription");
+
+		String html = "";
+		String message = "";
+
+		html = "<meta name=\"description\" content=\"foo\" />";
+		message = "expected: true - " + html;
+		setDocHeader(html);
+		assertTrue(message, extractor.hasMetaDescription());
+
+		html = "";
+		message = "expected: false - " + html;
+		setDocHeader(html);
+		assertFalse(message, extractor.hasMetaDescription());
+
+		html = "<meta name=\"description\" />";
+		message = "expected: false - " + html;
+		setDocHeader(html);
+		assertFalse(message, extractor.hasMetaDescription());
+	}
+
+	/**
+	 * Test of hasMetaKeywords method, of class ContentExtractor.
+	 */
+	@Test
+	public void testHasMetaKeywords() {
+		System.out.println("hasMetaKeywords");
+
+		String html = "";
+		String message = "";
+
+		html = "<meta name=\"keywords\" content=\"foo\" />";
+		message = "expected: true - " + html;
+		setDocHeader(html);
+		assertTrue(message, extractor.hasMetaKeywords());
+
+		html = "";
+		message = "expected: false - " + html;
+		setDocHeader(html);
+		assertFalse(message, extractor.hasMetaKeywords());
+
+		html = "<meta name=\"keywords\" />";
+		message = "expected: false - " + html;
+		setDocHeader(html);
+		assertFalse(message, extractor.hasMetaKeywords());
+	}
+
+	/**
+	 * Test of hasMetaCharset method, of class ContentExtractor.
+	 */
+	@Test
+	public void testHasMetaCharset() {
+		System.out.println("hasMetaCharset");
+
+		String html = "";
+		String message = "";
+
+
+		html = "<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />";
+		message = "expected: true - " + html;
+		setDocHeader(html);
+		assertTrue(message, extractor.hasMetaCharset());
+
+		html = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />";
+		message = "expected: true - " + html;
+		setDocHeader(html);
+		assertTrue(message, extractor.hasMetaCharset());
+
+		html = "";
+		message = "expected: false - " + html;
+		setDocHeader(html);
+		assertFalse(message, extractor.hasMetaCharset());
+
+		html = "<META http-equiv=\"Content-Type\" />";
+		message = "expected: false - " + html;
+		setDocHeader(html);
+		assertFalse(message, extractor.hasMetaCharset());
+	}
+
+	private void setDocHeader(String html) {
+		String htmlSkeleton = "<html><head></head><body></body></html>";
+		String baseUri = "http://example.com";
+		doc = Jsoup.parse(htmlSkeleton, baseUri);
+		this.doc.head().append(html);
+		this.doc.normalise();
+		this.extractor = new ContentExtractor(doc);
+
+	}
+}
