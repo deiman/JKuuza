af3258c97cf8c9d2d7b71489589e7a018ddc5b6f
Marek Pilecky <public@mefi100feles.cz>
2011-06-16 16:15:26 +0200

    Created view count_of_records_per_host in PageRepository

--- src/main/java/com/github/mefi/jkuuza/model/PageRepository.java
+++ src/main/java/com/github/mefi/jkuuza/model/PageRepository.java
@@ -1,9 +1,10 @@
 package com.github.mefi.jkuuza.model;
 
+import java.util.HashMap;
 import java.util.List;
 import org.ektorp.CouchDbConnector;
-import org.ektorp.support.CouchDbRepositorySupport;
-import org.ektorp.support.GenerateView;
+import org.ektorp.ViewResult;
+import org.ektorp.support.*;
 
 /**
  *
@@ -18,10 +19,23 @@ public class PageRepository extends CouchDbRepositorySupport<Page> {
 
 	@GenerateView
 	public List<Page> findByHost(String host) {
-                return queryView("by_host", host);
-        }
+		return queryView("by_host", host);
+	}
 
+	/**
+	 * Returns map where key is the host and value is count of records with this host
+	 *
+	 * @return map: key = host; value = count of records
+	 */
 
-	
+	@View(name = "count_of_records_per_host", map = "function(doc) { if(doc.docType == \"page\" && doc.host) {emit(doc.host, 1)}}", reduce = "function(keys, values) {return sum(values)}")
+	public HashMap<String, String> getCountOfRecordsPerHost() {
+		HashMap<String, String> map = new HashMap<String, String>();
 
+		ViewResult r = db.queryView(createQuery("count_of_records_per_host"));
+		for (ViewResult.Row row : r) {
+			map.put(row.getKey(), row.getValue());
+		}
+		return map;
+	}
 }
