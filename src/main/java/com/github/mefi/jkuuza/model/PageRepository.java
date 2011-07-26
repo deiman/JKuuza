6d74a794d4e7db2c4071765a91fb5a88626015ee
Marek Pilecky <public@mefi100feles.cz>
2011-07-26 16:37:31 +0200

    Fixed view in PageRepository

--- src/main/java/com/github/mefi/jkuuza/model/PageRepository.java
+++ src/main/java/com/github/mefi/jkuuza/model/PageRepository.java
@@ -1,8 +1,15 @@
 package com.github.mefi.jkuuza.model;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.IOUtils;
 import org.ektorp.CouchDbConnector;
+import org.ektorp.ViewQuery;
 import org.ektorp.ViewResult;
 import org.ektorp.support.*;
 
@@ -27,14 +34,19 @@ public class PageRepository extends CouchDbRepositorySupport<Page> {
 	 *
 	 * @return map: key = host; value = count of records
 	 */
-
 	@View(name = "count_of_records_per_host", map = "function(doc) { if(doc.docType == \"page\" && doc.host) {emit(doc.host, 1)}}", reduce = "function(keys, values) {return sum(values)}")
-	public HashMap<String, String> getCountOfRecordsPerHost() {
-		HashMap<String, String> map = new HashMap<String, String>();
+	public HashMap<String, Integer> getCountOfRecordsPerHost() {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
 
-		ViewResult r = db.queryView(createQuery("count_of_records_per_host"));
-		for (ViewResult.Row row : r) {
-			map.put(row.getKey(), row.getValue());
+		ViewQuery query = new ViewQuery()
+					.designDocId("_design/Page")
+					.viewName("count_of_records_per_host")
+					.reduce(true)
+					.group(true);
+		ViewResult r = db.queryView(query);
+		
+		for (ViewResult.Row row : r.getRows()) {
+			map.put(row.getKey(), Integer.parseInt(row.getValue()));
 		}
 		return map;
 	}
