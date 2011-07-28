a2fd9de67593cd68db80f79c48eae718ff54870d
Marek Pilecky <public@mefi100feles.cz>
2011-07-28 23:19:56 +0200

    Improved DbConnector

--- src/main/java/com/github/mefi/jkuuza/app/db/DbConnector.java
+++ src/main/java/com/github/mefi/jkuuza/app/db/DbConnector.java
@@ -12,20 +12,42 @@ import org.ektorp.impl.StdCouchDbInstance;
  */
 public class DbConnector {
 
-	private CouchDbConnector connector;	
+	private CouchDbConnector connector;
+
+	public DbConnector() {
+		HttpClient httpClient = new StdHttpClient.Builder().build();
+		String dbName = "jkuuza";
+		init(httpClient, dbName);
+	}
+
+	public DbConnector(String host, Integer port, String dbName) {
+		HttpClient httpClient = new StdHttpClient.Builder()
+					.host(host)
+					.port(port)
+					.build();
+		init(httpClient, dbName);
+	}
+
+	public DbConnector(String host, Integer port, String username, String password, String dbName) {
+		HttpClient httpClient = new StdHttpClient.Builder()
+					.host(host)
+					.port(port)
+					.username(username)
+					.password(password)
+					.build();
+		init(httpClient, dbName);
+	}	
 
 	public CouchDbConnector getConnection() {
-		if (connector == null) {
-			init();
-		}
 		return connector;
 	}
 
-	private void init() {
-		HttpClient httpClient = new StdHttpClient.Builder().build();
-		CouchDbInstance dbInstance = new StdCouchDbInstance(httpClient);
+	private void init(HttpClient httpClient, String dbName) {
+		HttpClient client = httpClient;
+		CouchDbInstance dbInstance = new StdCouchDbInstance(client);
 		// if the second parameter is true, the database will be created if it doesn't exists
-		this.connector = dbInstance.createConnector("jkuuza_test_database", true);
+		this.connector = dbInstance.createConnector(dbName, true);
+		System.setProperty("org.ektorp.support.AutoUpdateViewOnChange", "true");
 	}
 
 
