e34f2da62a0130b31b6a04814c0184e4857f10e7
Marek Pilecky <public@mefi100feles.cz>
2011-07-31 23:21:05 +0200

    DbConnector now throws exception

---
+++ src/main/java/com/github/mefi/jkuuza/app/db/CouchDbConnectionException.java
@@ -0,0 +1,18 @@
+package com.github.mefi.jkuuza.app.db;
+
+/**
+ *
+ * @author Marek Pilecky
+ */
+public class CouchDbConnectionException extends Exception {
+
+	public CouchDbConnectionException() {
+	}
+
+	public CouchDbConnectionException(String message) {
+		super(message);
+	}
+
+
+
+}
--- src/main/java/com/github/mefi/jkuuza/app/db/DbConnector.java
+++ src/main/java/com/github/mefi/jkuuza/app/db/DbConnector.java
@@ -1,5 +1,7 @@
 package com.github.mefi.jkuuza.app.db;
 
+import com.github.mefi.jkuuza.data.ConfigLoader;
+import java.io.IOException;
 import java.util.Properties;
 import org.ektorp.CouchDbConnector;
 import org.ektorp.CouchDbInstance;
@@ -21,49 +23,57 @@ public class DbConnector {
 		init(httpClient, dbName);
 	}
 
-	public DbConnector(Properties properties) {
+	public DbConnector(ConfigLoader configLoader) throws IOException, CouchDbConnectionException {
 
-		String host = !properties.getProperty("db_host").isEmpty() ? properties.getProperty("db_host") : "localhost" ;
-		Integer port = !properties.getProperty("db_port").isEmpty() ? Integer.parseInt(properties.getProperty("db_port")) : 5984;
-		String database = !properties.getProperty("db_database").isEmpty() ? properties.getProperty("db_database") : "jkuuza" ;
-		String username = !properties.getProperty("db_username").isEmpty() ? properties.getProperty("db_username") : "" ;
-		String password = !properties.getProperty("db_password").isEmpty() ? properties.getProperty("db_password") : "" ;
+		Properties properties = configLoader.load();
 
-		HttpClient httpClient = null;
-		if (username.isEmpty() || password.isEmpty()) {
+		String host = !properties.getProperty("db_host").isEmpty() ? properties.getProperty("db_host") : DefaultDbParams.HOST.toString();
+		Integer port = !properties.getProperty("db_port").isEmpty() ? Integer.parseInt(properties.getProperty("db_port")) : Integer.parseInt(DefaultDbParams.PORT.toString());
+		String database = !properties.getProperty("db_database").isEmpty() ? properties.getProperty("db_database") : DefaultDbParams.DATABASE.toString();
+		String username = !properties.getProperty("db_username").isEmpty() ? properties.getProperty("db_username") : DefaultDbParams.USERNAME.toString();
+		String password = !properties.getProperty("db_password").isEmpty() ? properties.getProperty("db_password") : DefaultDbParams.PASSWORD.toString();
+
+		try {
+			HttpClient httpClient = null;
 			httpClient = new StdHttpClient.Builder()
-					.host(host)
-					.port(port)
-					.build();
-		} else {
-			httpClient = new StdHttpClient.Builder()
-					.host(host)
-					.port(port)
-					.username(username)
-					.password(password)
-					.build();
+						.host(host)
+						.port(port)
+						.username(username)
+						.password(password)
+						.build();
+			init(httpClient, database);
+		} catch (Exception e) {
+			throw new CouchDbConnectionException();
 		}
-		
-		init(httpClient, database);
+
 	}
 
-	public DbConnector(String host, Integer port, String dbName) {
-		HttpClient httpClient = new StdHttpClient.Builder()
-					.host(host)
-					.port(port)
-					.build();
-		init(httpClient, dbName);
+	public DbConnector(String host, Integer port, String dbName) throws CouchDbConnectionException {
+
+		try {
+			HttpClient httpClient = new StdHttpClient.Builder()
+						.host(host)
+						.port(port)
+						.build();
+			init(httpClient, dbName);
+		} catch (Exception e) {
+			throw new CouchDbConnectionException();
+		}
 	}
 
-	public DbConnector(String host, Integer port, String username, String password, String dbName) {
-		HttpClient httpClient = new StdHttpClient.Builder()
+	public DbConnector(String host, Integer port, String username, String password, String dbName) throws CouchDbConnectionException {
+		try {
+			HttpClient httpClient = new StdHttpClient.Builder()
 					.host(host)
 					.port(port)
 					.username(username)
 					.password(password)
 					.build();
-		init(httpClient, dbName);
-	}	
+			init(httpClient, dbName);
+		} catch (Exception e) {
+			throw new CouchDbConnectionException();
+		}
+	}
 
 	public CouchDbConnector getConnection() {
 		return connector;
